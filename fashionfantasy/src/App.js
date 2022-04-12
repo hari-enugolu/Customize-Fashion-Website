c45c61de5726bf39f2394a5138090f8fbc938002
hari-enugolu <harienugolu123@gmail.com>
2022-04-12 22:43:17 +0530

    Shoeandbagspage

--- fashionfantasy/src/App.js
+++ fashionfantasy/src/App.js
@@ -3,6 +3,8 @@ import Header from "./Components/Header/Header";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import Filterbar from "./Components/Homepage/Filterbar";
+import Shoespage from "./Components/Shoespage/Shoespage";
+import Bagspage from "./Components/Bagspage/Bagspage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={[<Header />, <Filterbar />, <Homepage />]} />
+          <Route path="shoespage" element={Shoespage} />
+          <Route path="shoespage" element={Bagspage} />
         </Routes>
       </div>
     </BrowserRouter>
