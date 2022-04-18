20519702b4881a4bd6fd03f426e88c5a95758473
hari-enugolu <harienugolu123@gmail.com>
2022-04-18 13:02:22 +0530

    styling

--- fashionfantasy/src/Components/Homepage/Homepage.js
+++ fashionfantasy/src/Components/Homepage/Homepage.js
@@ -7,7 +7,7 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e28c9a14-b4c8-4c2e-98f0-959bbd1fd5e5/custom-nike-mercurial-superfly-8-academy-by-you.png"
             alt=""
             style={{ width: "300px" }}
           />
@@ -33,7 +33,7 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+            src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/182d5c8c-4da1-4dbc-8149-53e67face490/custom-nike-air-force-1-low-cozi-custom-shoe.png"
             alt=""
             style={{ width: "300px" }}
           />
@@ -59,7 +59,7 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c7a34d06-1fd5-4400-bb61-1ba50a14c70b/custom-nike-blazer-mid-77-by-you.png"
             alt=""
             style={{ width: "300px" }}
           />
@@ -85,7 +85,7 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b4d01bd2-7f65-4860-a8b4-fd5f36f08fae/custom-nike-air-force-1-mid-by-you-shoe.png"
             alt=""
             style={{ width: "300px" }}
           />
@@ -111,7 +111,7 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f6e3033f-400f-40b4-9b85-cfc93b18ce56/custom-nike-air-force-97-by-you.png"
             alt=""
             style={{ width: "300px" }}
           />
@@ -143,7 +143,33 @@ export default function Homepage() {
           />
         </div>
         <div className="contentBx">
-          <h2>Nike Shoes</h2>
+          <h2>Bags</h2>
+          {/* <div className="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div> */}
+          <div className="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <button> Customize</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://m.media-amazon.com/images/I/81EKq7XudPL._AC_UY327_FMwebp_QL65_.jpg"
+            alt=""
+            style={{ width: "200px", left: "20px", top: "20px" }}
+          />
+        </div>
+        <div className="contentBx">
+          <h2>Bags</h2>
           <div className="size">
             <h3>Size :</h3>
             <span>7</span>
@@ -165,11 +191,16 @@ export default function Homepage() {
           <img
             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ThsD_nRnv0sI0yY9dbxdbgHaKR%26pid%3DApi&f=1"
             alt=""
-            style={{ width: "200px" }}
+            style={{
+              width: "200px",
+              left: "20px",
+              top: "20px",
+              color: "white",
+            }}
           />
         </div>
         <div className="contentBx">
-          <h2>Nike Shoes</h2>
+          <h2>Bags</h2>
           <div className="size">
             <h3>Size :</h3>
             <span>7</span>
@@ -189,20 +220,20 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ThsD_nRnv0sI0yY9dbxdbgHaKR%26pid%3DApi&f=1"
+            src="https://m.media-amazon.com/images/I/81UyO3YJYKL._AC_UY327_FMwebp_QL65_.jpg"
             alt=""
-            style={{ width: "200px" }}
+            style={{ width: "200px", left: "20px", top: "20px" }}
           />
         </div>
         <div className="contentBx">
-          <h2>Nike Shoes</h2>
-          <div className="size">
+          <h2>Bags</h2>
+          {/* <div className="size">
             <h3>Size :</h3>
             <span>7</span>
             <span>8</span>
             <span>9</span>
             <span>10</span>
-          </div>
+          </div> */}
           <div className="color">
             <h3>Color :</h3>
             <span></span>
@@ -215,46 +246,20 @@ export default function Homepage() {
       <div className="card">
         <div className="imgBx">
           <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ThsD_nRnv0sI0yY9dbxdbgHaKR%26pid%3DApi&f=1"
+            src="https://m.media-amazon.com/images/I/61P8gcnN1SL._AC_UL480_FMwebp_QL65_.jpg"
             alt=""
-            style={{ width: "200px" }}
+            style={{ width: "200px", left: "20px", top: "20px" }}
           />
         </div>
         <div className="contentBx">
-          <h2>Nike Shoes</h2>
-          <div className="size">
+          <h2>Hand Bag</h2>
+          {/* <div className="size">
             <h3>Size :</h3>
             <span>7</span>
             <span>8</span>
             <span>9</span>
             <span>10</span>
-          </div>
-          <div className="color">
-            <h3>Color :</h3>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <button> Customize</button>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ThsD_nRnv0sI0yY9dbxdbgHaKR%26pid%3DApi&f=1"
-            alt=""
-            style={{ width: "200px" }}
-          />
-        </div>
-        <div className="contentBx">
-          <h2>Nike Shoes</h2>
-          <div className="size">
-            <h3>Size :</h3>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
-            <span>10</span>
-          </div>
+          </div> */}
           <div className="color">
             <h3>Color :</h3>
             <span></span>
